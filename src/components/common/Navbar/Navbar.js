4460bec80ba74aefac0e73faee500a6b9fb8473b
Lemeri123 <lemerigift@gmail.com>
2025-04-25 04:01:20 +0300

    Made changes

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Navbar.css';
-
 function Navbar() {
   return (
     <div className="navbar">
-      <span className="logo">LEMERI</span>
-      <div className="menu-icon">
-        <div></div>
-        <div></div>
-      </div>
+    <span className="logo">LEMERI</span>
+    <div className="menu-icon">
+      <div></div>
+      <div></div>
     </div>
+  </div>
   );
 }
 export default Navbar;
